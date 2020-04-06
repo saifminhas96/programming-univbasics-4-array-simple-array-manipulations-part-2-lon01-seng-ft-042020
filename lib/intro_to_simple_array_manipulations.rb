bdd587a60d2ef4d0d3ab8fd1034a582210dc3fd7
Saif Minhas <saif_minhas@hotmail.com>
2020-04-06 11:33:45 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+def using_concat(arr1,arr2)
+  arr1.concat (arr2) 
+end 
+
+def using_insert(arr,word)
+ arr.insert(4,'Python')
+end 
+
+def using_uniq(array)
+  array.uniq 
+end 
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(arr,word)
+  arr.delete("Steven")
+end
+
+def using_delete_at(array,integer)
+  array.delete_at(2)
+end
+  
